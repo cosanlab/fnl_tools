1cda4c51a6224ab4e3e0c95786e1ea82c5dcb3eb
Luke Chang <lukechang@MacBook-Pro-8.local>
2018-09-10 12:58:27 -0400

    added helper functions to utilities.

--- fnl_tools/utils.py
+++ fnl_tools/utils.py
@@ -27,3 +27,30 @@ def get_rect_coord(labels):
 def rec_to_time(values, TR=2.):
     times = np.array(values)/60.*TR
     return [str(int(np.floor(t)))+':'+str(int((t-np.floor(t))*60)) for t in times]
+
+def create_hmm_labels(data, threshold=0.5):
+    hmmdat = pd.DataFrame(binarize(data, threshold=0.5, copy=True))
+    return pd.DataFrame([hmmdat[c[0]]*(i+1) for i,c in enumerate(hmmdat.iteritems())]).sum()
+
+def clean(dm, replace_na=True):
+    ''' This function removes nans and drops any duplicate columns
+    '''
+    if replace_na:
+        dm.fillna(value=0, inplace=True)
+    keep = []; remove = []
+    for i, c in dm.iteritems():
+        for j, c2 in dm.iteritems():
+            if i != j:
+                r = pearsonr(c,c2)[0]
+                if (r > 0.99) & (j not in keep):
+                    keep.append(i)
+                    remove.append(j)
+    dm.drop(remove, inplace=True, axis=1)
+    return dm
+    
+def expand_states(data):
+    ''' Given an input of vector labels from HMM, expand into separate variables'''
+    out = {}
+    for i in sorted(data.unique()):
+        out[i] = data==i
+    return pd.DataFrame(out)
